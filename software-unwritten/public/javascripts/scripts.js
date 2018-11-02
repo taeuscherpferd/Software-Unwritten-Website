45d9f7e451faf952bdf164ece1f258ae75de09c6
root <root@ip-172-31-22-204.us-west-2.compute.internal>
2018-11-02 17:20:44 +0000

    Fixed a couple of bugs

--- software-unwritten/public/javascripts/scripts.js
+++ software-unwritten/public/javascripts/scripts.js
@@ -3,12 +3,12 @@ var app = angular.module("myApp", []);
 app.controller('myCtrl', function($scope, $http) {
   $scope.onclick = function(button) {
     var value = $("#txtBx").val();
+    var prompts = ["1. poisonous",]
     if (value <= 31 && value >= 1) {
       var selectedImage = "<img src=\"images/inktober2018/day" + value + ".jpeg\" height=\"500\" width=\"600\">";
     }
     else {
-      selectedImage = "<img src=\"\" alt=\"Out of Bounds (Select 1-31)\">";
-      
+      selectedImage = "<img src=\"images/inktober2018/prompts.jpeg\" height=\"500\" width=\"600\" alt=\"Out of Bounds (Select 1-31)\">";
     }
     
     $("#sketch").html(selectedImage);
